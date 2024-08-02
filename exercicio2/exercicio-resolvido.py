f26ed6b268c5dd80afeb6c2bcdd44d7b1b419473
Daniela2319 <frankvt04@gmail.com>
2024-08-02 20:55:50 -0300

    desafio 1 - exercicio_02

--- exercicio2/exercicio-resolvido.py
+++ exercicio2/exercicio-resolvido.py
@@ -1,5 +1,5 @@
 # 1 - Desafio: exercicio 2
-"""2. Peça ao usuário para informar o ano de nascimento. Em seguida, calcule e imprima a idade atual."""
+""" 2. Peça ao usuário para informar o ano de nascimento. Em seguida, calcule e imprima a idade atual. """
 anoNascimento = int(input("Digite o ano de nascimento: "))
 anoAtual = 2024
 
